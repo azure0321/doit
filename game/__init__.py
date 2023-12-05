f4ba02071cb4d293dded48054bee79522d3c2ed8
ny_980321 <ny_980321@naver.com>
2023-12-05 14:39:17 +0900

    first commit

---
+++ game/__init__.py
@@ -0,0 +1,17 @@
+VERSION = 3.5
+
+def version_info():
+    print(f"The version of this game is {VERSION}.")
+
+from.graphic.render import render_test
+VERSION = 3.5
+def print_version_info():
+    print(f"The version of this game is {VERSION}")
+    
+from.graphic.render import render_test
+VERSION = 3.5
+def print_version_info() 
+    print(f"The version of this game is {VERSION}")
+
+print("Initalizing game ...")    
+
---
+++ game/graphic/render.py
@@ -0,0 +1,12 @@
+def render_test():
+    print("render")
+    
+from game.sound.echo import echo_test
+def render_test():
+    print("render")
+    echo_test() 
+    
+from .. sound.echo import echo_test
+def render_test():
+    print("render")
+    echo_test()     
